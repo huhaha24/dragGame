03ed27d8415d03f6d5bd3e1fbdc82661d2b5daa7
huhaha24 <1683197534@qq.com>
2021-05-18 21:11:57 +0800

    Add files via upload
    增加文件

---
+++ foodList.js
@@ -0,0 +1,158 @@
+const AREA_TYPE = {
+  COLD: '冷藏区',
+  VARIABLE: '变温区',
+  FROZEN: '冷冻区',
+  NOT: '不放冰箱',
+  CASUAL: '放不放都可以',
+  RIGHT: '冷藏右边区域'
+}
+
+const FOOD_LIST = [
+  {
+    name: '苹果',
+    id: 'pingguo',
+    area: AREA_TYPE.COLD
+  },
+  {
+    name: '樱桃',
+    id: 'yingtao',
+    area: AREA_TYPE.COLD
+  },
+  {
+    name: '葡萄',
+    id: 'putao',
+    area: AREA_TYPE.COLD
+  },{
+    name: '白菜',
+    id: 'baicai',
+    area: AREA_TYPE.COLD
+  },{
+    name: '西兰花',
+    id: 'xilanhua',
+    area: AREA_TYPE.COLD
+  },{
+    name: '大蒜',
+    id: 'dasuan',
+    area: AREA_TYPE.COLD
+  },{
+    name: '蒜薹',
+    id: 'suantai',
+    area: AREA_TYPE.COLD
+  },{
+    name: '松花蛋',
+    id: 'songhuadan',
+    area: AREA_TYPE.COLD
+  },{
+    name: '鲜牛奶',
+    id: 'xianniunai',
+    area: AREA_TYPE.COLD
+  },{
+    name: '香菇',
+    id: 'xianggu',
+    area: AREA_TYPE.COLD
+  },{
+    name: '豆芽',
+    id: 'douya',
+    area: AREA_TYPE.COLD
+  },{
+    name: '甜玉米',
+    id: 'tianyumi',
+    area: AREA_TYPE.COLD
+  },{
+    name: '板栗',
+    id: 'banli',
+    area: AREA_TYPE.COLD
+  },{
+    name: '荔枝',
+    id: 'lizhi',
+    area: AREA_TYPE.COLD
+  },{
+    name: '虾皮',
+    id: 'xiapi',
+    area: AREA_TYPE.COLD
+  },{
+    name: '海带',
+    id: 'haidai',
+    area: AREA_TYPE.COLD
+  },{
+    name: '橙子',
+    id: 'chengzi',
+    area: AREA_TYPE.COLD
+  },{
+    name: '甜瓜',
+    id: 'tiangua',
+    area: AREA_TYPE.COLD
+  },{
+    name: '豌豆',
+    id: 'wadou',
+    area: AREA_TYPE.COLD
+  },{
+    name: '荸荠',
+    id: 'biqi',
+    area: AREA_TYPE.COLD
+  },{
+    name: '奶油蛋糕',
+    id: 'dangao',
+    area: AREA_TYPE.COLD
+  },{
+    name: '鲜肉块',
+    id: 'xianroukuai',
+    area: AREA_TYPE.VARIABLE
+  },{
+    name: '熟肉',
+    id: 'shurou',
+    area: AREA_TYPE.VARIABLE
+  },{
+    name: '冻肉',
+    id: 'dongrou',
+    area: AREA_TYPE.FROZEN
+  },{
+    name: '冻鱼',
+    id: 'dongyu',
+    area: AREA_TYPE.FROZEN
+  },{
+    name: '冰淇淋',
+    id: 'icecream',
+    area: AREA_TYPE.FROZEN
+  },{
+    name: '速冻汤圆',
+    id: 'tangyuan',
+    area: AREA_TYPE.FROZEN
+  },{
+    name: '山药',
+    id: 'shanyao',
+    area: AREA_TYPE.NOT
+  },{
+    name: '生姜',
+    id: 'shengjiang',
+    area: AREA_TYPE.NOT
+  },{
+    name: '芒果',
+    id: 'mangguo',
+    area: AREA_TYPE.NOT
+  },{
+    name: '柠檬',
+    id: 'ningmeng',
+    area: AREA_TYPE.NOT
+  },{
+    name: '黄瓜',
+    id: 'huanggua',
+    area: AREA_TYPE.CASUAL
+  },{
+    name: '西葫芦',
+    id: 'xihulu',
+    area: AREA_TYPE.CASUAL
+  },{
+    name: '番茄',
+    id: 'fanqie',
+    area: AREA_TYPE.CASUAL
+  },{
+    name: '茄子',
+    id: 'qiezi',
+    area: AREA_TYPE.CASUAL
+  },{
+    name: '辣椒',
+    id: 'lajiao',
+    area: AREA_TYPE.CASUAL
+  },
+]
